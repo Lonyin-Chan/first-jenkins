e6adc2d79ad25f07957b2f12fce230bea0073595
Lonyin_Chan <36819051+Lonyin-Chan@users.noreply.github.com>
2024-11-04 11:50:33 +0000

    Update starwars.sh

--- starwars.sh
+++ starwars.sh
@@ -1,3 +1,12 @@
-#!/bin/bash
-
-telnet towel.blinkenlights.nl
+#!/usr/bin/expect
+spawn telnet towel.blinkenlights.nl
+expect {
+    "Escape character is '^]'." {
+        send_user "\nConnected!\n"
+        interact
+    }
+    timeout {
+        send_user "\nConnection timed out\n"
+        exit
+    }
+}
